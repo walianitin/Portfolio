35dce85b12e52df94db0de6613d98a279892e96a
walianitin <walianitin828@gmail.com>
2025-09-14 11:54:37 +0530

    added changes to footer

--- components/Education.tsx
+++ components/Education.tsx
@@ -16,7 +16,7 @@ const educationData: EducationItem[] = [
     institution: 'National Institute of Technology, Kurukshetra',
     degree: 'B.Tech',
     department: 'Production Engineering (Mechanical)',
-    duration: '2022 - 2026 (Expected)',
+    duration: '2022 - 2026 ',
     logo: '/nitkkr-log.png' // Replace with your image path
   }
 ];
@@ -25,7 +25,7 @@ const EducationCard = () => (
   <div className="max-w-2xl mx-auto p-4  ">
     <h2 className="text-4xl text-center font-bold mb-6">Education</h2>
     
-    <div className=" flex flex-row bg-red-600 justify-center items-centre  ">
+    <div className=" flex flex-row justify-center items-centre  ">
       {educationData.map((edu, index) => (
         <MagicCard 
           key={index}
@@ -37,9 +37,9 @@ const EducationCard = () => (
           <div className="flex-1 min-w-0">
             <div className="flex flex-col sm:flex-row sm:justify-between sm:items-baseline gap-1">
               <h3 className="text-lg font-bold truncate">{edu.institution}</h3>
-              <span className="text-neutral-900 text-xs whitespace-nowrap pl-20 ">{edu.duration}</span>
+              <span className=" text-xs whitespace-nowrap pl-20 ">{edu.duration}</span>
 
-           <div className="flex gap-2 text-sm mt-1 text-neutral-900">
+           <div className="flex gap-2 text-sm mt-1 ">
               <span>{edu.degree}</span>
               <span className="text-neutral-400">|</span>
               <span className=" truncate">{edu.department}</span>
@@ -50,8 +50,8 @@ const EducationCard = () => (
             src={edu.logo} 
             alt={edu.institution} 
             width={40} 
-            height={40} 
-            className="rounded-sm object-contain"
+            height={40}
+            className="rounded-full object-contain "
           />
         
         </MagicCard>
